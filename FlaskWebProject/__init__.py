6327fb97b70793ed7bc4cbc0266a40df8c243444
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-05-15 19:12:35 +0200

    logging-streamhandler-and-standard-streams

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,6 +2,7 @@
 The flask application package.
 """
 import logging
+import sys
 
 from flask import Flask
 from flask_login import LoginManager
@@ -14,9 +15,23 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-gunicorn_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
+MIN_LOGGING_LEVEL = logging.INFO
+stdout_stream_handler = logging.StreamHandler(sys.stdout)
+stderr_stream_handler = logging.StreamHandler(sys.stderr)
+
+# messages lower than WARNING go to stdout
+# messages >= WARNING (and >= STDOUT_LOG_LEVEL) go to stderr
+stdout_stream_handler.setLevel(MIN_LOGGING_LEVEL)
+stdout_stream_handler.setLevel(max(MIN_LOGGING_LEVEL, logging.WARNING))
+
+#rootLogger = logging.getLogger()
+#rootLogger.addHandler(stdout_stream_handler)
+#rootLogger.addHandler(stdout_stream_handler)
+#app.logger = logging.getLogger(__name__)
+
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(stdout_stream_handler)
+app.logger.addHandler(stderr_stream_handler)
 
 Session(app)
 db = SQLAlchemy(app)
