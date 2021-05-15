e32f4f8746ae0dec085d84109096e1fadd9cfa4f
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-05-15 17:25:29 +0200

    FIX - Make flask log to stdout instead of stderr

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -3,6 +3,7 @@ The flask application package.
 """
 import logging
 import sys
+from logging.config import dictConfig
 
 from flask import Flask
 from flask_login import LoginManager
@@ -10,6 +11,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 from flask_session import Session
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
 
 app = Flask(__name__)
 app.config.from_object(Config)
