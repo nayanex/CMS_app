b1b388cc273eb086f88e11eaea93643b59ddc294
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-05-15 19:50:06 +0200

    cryinggggggit add . LOL

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,10 +11,19 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_session import Session
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+logging_dict = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+        }
+    },
+}
+
+logging.config.dictConfig(logging_dict)
 MIN_LOGGING_LEVEL = logging.INFO
 app.logger.setLevel(logging.INFO)
 
