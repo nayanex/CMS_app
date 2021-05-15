05e85cbc1dcc7213bf646f016ba6d055e3ff33a8
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-05-15 18:31:59 +0200

    FIX stderr stdout issue

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,8 +2,6 @@
 The flask application package.
 """
 import logging
-import sys
-from logging.config import dictConfig
 
 from flask import Flask
 from flask_login import LoginManager
@@ -12,30 +10,12 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_session import Session
 
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-            }
-        },
-        "handlers": {
-            "wsgi": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://sys.stdout",
-                "formatter": "default",
-            }
-        },
-        "root": {"level": "INFO", "handlers": ["wsgi"]},
-    }
-)
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 app.logger.setLevel(logging.INFO)
-streamHandler = logging.StreamHandler(stream=sys.stdout)
+streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
 
